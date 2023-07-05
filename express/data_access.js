44959df2d8c18d5ff6ece1aa24bdab1b6a476db1
Sumbul Vohra <svohra2@travelers.com>
2023-07-05 11:12:34 -0400

    Changes to calls

--- express/data_access.js
+++ express/data_access.js
@@ -10,6 +10,11 @@ const client = new MongoClient(url);
 // Database and collection variables
 const dbName = "swapi";
 const filmsCollection = "films"
+const planetsCollection = "planets"
+const charactersCollection = "characters"
+const films_planets = "films_planets"
+const films_characters = "films_characters"
+
 
 // /api/characters              findAllCharacters
 // /api/films                   findAllFilms
@@ -34,11 +39,55 @@ module.exports.call = async function call(operation, parameters, callback) {
     const db = client.db(dbName);
     // set the collection to use
     const collection = db.collection(filmsCollection);
+    const collection2 = db.collection(planetsCollection);
+    const collection3 = db.collection(charactersCollection);
+    const collection4 = db.collection(films_planets);
+    const collection5 = db.collection(films_characters);
 
     switch (operation.toLowerCase()) {
+        case 'findallcharacters':
+            const characters = await collection.find({}).toArray();
+            callback({ characters: characters });
+            break;
         case 'findallfilms':
             const films = await collection.find({}).toArray();
             callback({ films: films });
+            break;
+        case 'findallplanets':
+            const planets = await collection.find({}).toArray();
+            callback({ planets: planets });
+            break;
+        case 'findcharacterbyid':
+            const characterById = await collection.find({}).toArray();
+            callback({ characterById: characterById });
+            break;
+        case 'findfilmbyid':
+            const filmById = await collection.find({}).toArray();
+            callback({ filmById: filmById });
+            break;
+        case 'findplanetbyid':
+            const planetById = await collection.find({}).toArray();
+            callback({ planetById: planetById });
+            break;
+        case 'findcharactersbyfilm':
+            const charactersByFilm = await collection.find({}).toArray();
+            callback({ charactersByFilm: charactersByFilm });
+            break;
+        case 'findplanetsbyfilm':
+            const planetsByFilm = await collection.find({}).toArray();
+            callback({ planetsByFilm: planetsByFilm });
+            break;
+        case 'findfilmsbycharacter':
+            const filmsByCharacter = await collection.find({}).toArray();
+            callback({ filmsByCharacter: filmsByCharacter });
+            break;
+        case 'findfilmsbyplanet':
+            const filmsByPlanet = await collection.find({}).toArray();
+            callback({ filmsByPlanet: filmsByPlanet });
+            break;
+        case 'findcharactersbyplanet':
+            const charactersByPlanet = await collection.find({}).toArray();
+            callback({ charactersByPlanet: charactersByPlanet });
             break;
         default:
             break;
