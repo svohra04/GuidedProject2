9c6eec40758f9a3a73ac921d1db9dd6f6b19584a
Brandon Chu <bgc7753@g.rit.edu>
2023-07-05 09:28:32 -0400

    Add function call names for mongo

--- express/data_access.js
+++ express/data_access.js
@@ -11,6 +11,21 @@ const client = new MongoClient(url);
 const dbName = "swapi";
 const filmsCollection = "films"
 
+// /api/characters              findAllCharacters
+// /api/films                   findAllFilms
+// /api/planets                 findAllPlanets
+// /api/characters/:id          findCharacterById
+// /api/films/:id               findFilmById
+// /api/planets/:id             findPlanetById
+// /api/films/:id/characters    findCharactersByFilm
+// /api/films/:id/planets       findPlanetsByFilm
+// /api/characters/:id/films    findFilmsByCharacter
+// /api/planets/:id/films       findFilmsByPlanet
+// /api/planets/:id/characters  FindCharactersByPlanet
+
+// CamelCase for server.js but all lowercase for data_access
+
+
 module.exports.call = async function call(operation, parameters, callback) {
     // connect to the db server
     await client.connect();
